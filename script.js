f64cc5970d69497aa5f97a37bcc1fd819ce3fe98
thecrownedsquid(nat) <natnatdap@gmail.com>
2017-01-11 21:11:28 +0000

    grid

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//Write your function calls below this line
+//Write your function calls below this line  
+setColor (2,3,"black");
+setColor (2,6,"black");
+setColor (0,0,"blue");
+setColor (0,9,"blue");
+setColor (9,9,"blue");
+setColor (9,0,"blue");
+setColor (5,2,"black");
+setColor (6,3,"black");
+setColor (6,4,"black");
+setColor (6,5,"black");
+setColor (2,5,"red");
+setColor(2,4,"red");
+setColor(3,3,"red");
+setColor(4,3,"red");
+setColor(4,4,"red");
+setColor(4,5,"red");
+setColor(4,6,"red");
+setColor(5,6,"red");
+setColor(6,6,"red");
+setColor(9,2,"Purple");
+setColor(8,2,"purple");
+setColor(7,2,"purple");
+setColor(6,2,"purple");
